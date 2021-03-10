909750fc8e38ee5ddc7de1a870911f020862dd84
Berthelmaster <berthel0802@gmail.com>
2021-03-10 11:06:04 +0100

    updated

--- src/UI/App.java
+++ src/UI/App.java
@@ -1,6 +1,7 @@
 package UI;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class App {
     private JButton button1;
@@ -9,7 +10,10 @@ public class App {
     public App() {
         JFrame frame = new JFrame("App");
         frame.setContentPane((this.panelMain));
+        frame.setLocationRelativeTo(null);
+        frame.setPreferredSize(new Dimension(600, 450));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationByPlatform(true);
         frame.pack();
         frame.setVisible(true);
     }
