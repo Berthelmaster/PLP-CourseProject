7390916e381e9a05e2c9588aa31b353be1cd3300
Berthelmaster <berthel0802@gmail.com>
2021-04-24 15:32:42 +0200

    added Unit to each draw

--- src/Scala/Draw.scala
+++ src/Scala/Draw.scala
@@ -22,7 +22,7 @@ class Draw {
     // START ALL ELSE GOES HERE
 
     // Figure out what class to call
-    DrawFromString(head, tail)
+    DrawFromString(head, tail, null)
 
 
 
@@ -42,31 +42,31 @@ class Draw {
     case _ => output
   }
 
-  def DrawLine(arr: Array[String], output: Array[String]): Array[String] = {
-    
-  }
-
-  def DrawRectangle(arr: Array[String], output: Array[String]): Array[String] = {
+  def DrawLine(arr: Array[String], output: Array[String]): Unit = {
 
   }
 
-  def DrawCircle(arr: Array[String], output: Array[String]): Array[String] = {
+  def DrawRectangle(arr: Array[String], output: Array[String]): Unit = {
 
   }
 
-  def DrawText(arr: Array[String], output: Array[String]): Array[String] = {
+  def DrawCircle(arr: Array[String], output: Array[String]): Unit = {
 
   }
 
-  def DrawBounding(arr: Array[String], output: Array[String]): Array[String] = {
+  def DrawText(arr: Array[String], output: Array[String]): Unit = {
 
   }
 
-  def DrawObject(arr: Array[String], output: Array[String]): Array[String] = {
+  def DrawBounding(arr: Array[String], output: Array[String]): Unit = {
 
   }
 
-  def DrawFill(arr: Array[String], output: Array[String]): Array[String] = {
+  def DrawObject(arr: Array[String], output: Array[String]): Unit = {
+
+  }
+
+  def DrawFill(arr: Array[String], output: Array[String]): Unit = {
 
   }
 
